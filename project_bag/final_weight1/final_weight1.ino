4fe84db37a665717fcb0fae0031d732f0db893bf
viren <virendra@fabfresh.in>
2016-02-06 02:45:25 +0530

    angularLogin

--- project_bag/final_weight1/final_weight1.ino
+++ project_bag/final_weight1/final_weight1.ino
@@ -1,16 +1,20 @@
+#include<Sleep_n0m1.h>
 #include "HX711.h"
 
-#define calibration_factor -13110.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
+#define calibration_factor -13110.0 
 
 #define DOUT  3
 #define CLK  2
 
-HX711 scale(DOUT, CLK);
 
+HX711 scale(DOUT, CLK);
+//Sleep sleep;
+//unsigned long sleepTime;
 void setup() {
   Serial.begin(9600);
- pinMode(5,OUTPUT);
+ //sleepTime=  10000;
  pinMode(6,OUTPUT);
+ 
 
   scale.set_scale(calibration_factor); 
   scale.tare();	
@@ -20,20 +24,22 @@ void setup() {
 
 void loop() {
  
-  delay(200);
+  
+ 
   float w = abs(scale.get_units());
   w = w*453.59237 ;
   Serial.println(w);
-  if(w >=2000 )
+  if(w >=3000 )
   { 
-    digitalWrite(5,HIGH);
     digitalWrite(6,HIGH);
     }
     else
-    { digitalWrite(5,LOW);
+    { 
       digitalWrite(6,LOW);
     }
- delay(200); 
- Serial.println();
+ delay(10000); 
+
+ //sleep.pwrDownMode();
+ //sleep.sleepDelay(sleeptime);
  
 }
